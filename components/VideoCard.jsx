50d6bbbf752cb5b3013583649a57decf2ea9dfb2
Rakejogers <87666498+Rakejogers@users.noreply.github.com>
2024-05-27 22:01:47 -0500

    update videocard

--- components/VideoCard.jsx
+++ components/VideoCard.jsx
@@ -24,8 +24,8 @@ const VideoCard = ({ video: {title, thumbnail, video, creator: {username, avatar
         </View>
         {play ? (
               <Video
-                  source={{ uri: item.video }}
-                  className="w-52 h-72 rounded-35 mt-3 bg-white/10"
+                  source={{ uri: video }}
+                  className="w-full h-60 rounded-xl mt-3"
                   resizeMode={ResizeMode.CONTAIN}
                   useNativeControls
                   shouldPlay
